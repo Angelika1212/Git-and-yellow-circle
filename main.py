a7f145dea595b25dd4541a0c24042fa2e06f11cf
ubuntu12 <zanzelika@yandex.ru>
2019-11-16 17:34:14 +0300

    + class

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 import sys
-from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
 from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel
 
 
-class MyWidget(QMainWindow):
+class Example(QWidget):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
-        self.pushButton.clicked.connect(self.run)
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300, 300, 300, 300)
+        self.btn = QPushButton('Кнопка', self)
+        self.btn.resize(100, 100)
+        self.btn.move(100, 100)
+        self.label = QLabel(self)
+        self.label.move(80, 30)
+        self.btn.clicked.connect(self.run)
 
     def run(self):
         self.pixmap = QPixmap('Безымянный.png')
         self.label.setPixmap(self.pixmap)
-        self.label.resize(500, 500)
 
 
-app = QApplication(sys.argv)
-ex = MyWidget()
-ex.show()
-sys.exit(app.exec_())
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
